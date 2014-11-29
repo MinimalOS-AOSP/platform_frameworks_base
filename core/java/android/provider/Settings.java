c55adfcc82ce031a5d275be6bd0268deeeacac78
Muhammed Nazim <mnm9994u@gmail.com>
2014-11-29 21:43:37 +0100

    Forward Port: Volume button seek on screen off [1/2]
    PS : Fix the short press event type
    
    Adapted for Marshmallow by zephiK
    
    Change-Id: Id55a1240a133b5144502fced862bae040b390662

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3294,6 +3294,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
         /**
+         * Volume music controls
+         * @hide
+         */
+        public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+      
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
