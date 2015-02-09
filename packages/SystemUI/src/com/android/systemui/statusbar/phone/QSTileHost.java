54ec87d1eb0e057f0f93161f71fc39831e23542a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:48:03 +0000

    QS: add Screen off tile
    * Works with SystemUI tuner
    * Adapted for M by KreAch3R
    * Long press for power menu
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ib40968ed38edeb26fa657c69106d446c6ebdb39e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -256,6 +257,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("location")) return new LocationTile(this);
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
+        else if (tileSpec.equals("screenoff")) return new ScreenOffTile(this);
         else if (tileSpec.equals("timeout")) return new ScreenTimeoutTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
