01d4046f473e573db85b58eed18c12d40f959f67
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2015-10-22 23:44:50 +0530

    data/sounds: Use Google's Audio [1/2] Been working on data/sounds forlder for much time now. It refuses to get de-bloated. Now that I've got my hands on Nexus 6 MRA58K Image, the Google's Audio files can finally be added here :)
    * Created a new makefile (GoogleAudioPackage.mk) for this.
    
    By @dark-falcon
    Signed-off-by: Chinmay Kunkikar <chinmay.kunkikar@gmail.com>

---
+++ data/sounds/GoogleAudioPackage.mk
@@ -0,0 +1,35 @@
+# (C) 2015 The Minimal OS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#Definitions
+LOCAL_PATH := frameworks/base/data/sounds
+ALARM_FILES := Argon Carbon Helium Krypton Neon Oxygen Osmium Platinum
+EFFECT_FILES := Dock Effect_Tick KeypressDelete KeypressInvalid KeypressReturn KeypressSpacebar KeypressStandard Lock \
+                LowBattery NFCFailure NFCInitiated NFCSuccess NFCTransferComplete NFCTransferInitiated Trusted \
+                Undock Unlock VideoRecord VideoStop WirelessChargingStarted audio_end audio_initiate camera_click camera_focus
+NOTIFICATION_FILES := Ariel Carme Ceres Elara Europa Iapetus Io Rhea Salacia Tethys Titan
+RINGTONE_FILES := Callisto Dione Ganymede Luna Oberon Phobos RobotsforEveryone Sedna SpagnolaOrchestration Titania Triton Umbriel
+
+#Copy sounds
+PRODUCT_COPY_FILES += $(foreach sound,$(ALARM_FILES),\
+    $(LOCAL_PATH)/material/alarms/ogg/$(sound).ogg:system/media/audio/alarms/$(sound).ogg)
+
+PRODUCT_COPY_FILES += $(foreach sound,$(EFFECT_FILES),\
+    $(LOCAL_PATH)/material/effects/ogg/$(sound).ogg:system/media/audio/ui/$(sound).ogg)
+
+PRODUCT_COPY_FILES += $(foreach sound,$(NOTIFICATION_FILES),\
+    $(LOCAL_PATH)/material/notifications/ogg/$(sound).ogg:system/media/audio/notifications/$(sound).ogg)
+
+PRODUCT_COPY_FILES += $(foreach sound,$(RINGTONE_FILES),\
+    $(LOCAL_PATH)/material/ringtones/ogg/$(sound).ogg:system/media/audio/ringtones/$(sound).ogg)
