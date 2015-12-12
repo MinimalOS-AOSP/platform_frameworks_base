94b4fb2f640bd8b78932708407d0bfc3f45d64e4
Maunik Shah <mshah@codeaurora.org>
2015-12-12 15:28:51 +0530

    UsbdeviceManager: Avoid sending extra USB_STATE broadcast intent
    Multiple broadcast intents to start MTPService is putting
    MTPServer in bad state. So avoiding extra USB_STATE broadcast
    intent.
    
    Change-Id: I1b38c71d6c175c4c01d839005854accd57419e95

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -426,7 +426,6 @@ public class UsbDeviceManager {
             if (DEBUG) Slog.d(TAG, "setUsbDataUnlocked: " + enable);
             mUsbDataUnlocked = enable;
             updateUsbNotification();
-            updateUsbStateBroadcast();
             setEnabledFunctions(mCurrentFunctions, true);
         }
 
